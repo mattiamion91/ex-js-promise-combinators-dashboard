15be1803663f800e90d10b890dfa05191ced075e
Mattia Mion <mattia.mion@gmail.com>
2026-04-09 16:08:25 +0200

    feat: bonus1

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ London is in United Kingdom.
 Today there are 18 degrees and the weather is Partly cloudy.
 The main airport is London Heathrow Airport.
 🎯 Bonus 1 - Risultato vuoto
-Se l’array di ricerca è vuoto, invece di far fallire l'intera funzione, semplicemente i dati relativi a quella chiamata verranno settati a null e  la frase relativa non viene stampata. Testa la funzione con la query “vienna” (non trova il meteo).
+Se l’array di ricerca è vuoto, invece di far fallire l'intera funzione, semplicemente i dati relativi a quella chiamata verranno settati a null e  la messaggio relativa non viene stampata. Testa la funzione con la query “vienna” (non trova il meteo).
 // Risposta API
 {
   city: "Vienna",
@@ -75,33 +75,44 @@ const getDashboardData = async (query) => {
 
     try {
         const promiseDestinations = fetchJson(`${API_URL}/destinations?search=${query}`);
-        const promiseWeather = fetchJson(`${API_URL}/weathers?search=${query}`);
-        const promiseAirport = fetchJson(`${API_URL}/airports?search=${query}`);
+        const promiseWeathers = fetchJson(`${API_URL}/weathers?search=${query}`);
+        const promiseAirports = fetchJson(`${API_URL}/airports?search=${query}`);
 
-        const promises = [promiseDestinations, promiseWeather, promiseAirport];
-        const [destinations, weather, airport] = await Promise.all(promises);
+        const promises = [promiseDestinations, promiseWeathers, promiseAirports];
+        const [destinations, weathers, airports] = await Promise.all(promises);
+
+        const destination = destinations[0]
+        const weather = weathers[0]
+        const airport = airports[0]
 
         return {
-            city: destinations[0].name,
-            country: destinations[0].country,
-            temperature: weather[0].temperature,
-            weather: weather[0].weather_description,
-            airport: airport[0].name
+            city: destination ? destination.name : null,
+            country: destination ? destination.country : null,
+            temperature: weather ? weather.temperature : null,
+            weather: weather ? weather.weather_description : null,
+            airport: airport ? airport.name : null
         }
 
     } catch (error) {
-        throw new Error('errore recupero dati');
+        throw new Error(`errore recupero dati: ${error.messagge}`);
     }
 
 }
 
-//uso la funzione appena creata
-(async () => {
-    const data = await getDashboardData('paris')
-    console.log(`dashboard: `, data);
-    console.log(`
-        ${data.city} si trova in ${data.country}
-        oggi ci sono ${data.temperature} gradi e il clima é ${data.weather}
-        l'aeroporto principale é ${data.airport}
-        `);
-})()
+//uso la funzione
+getDashboardData('vienna')
+    .then(data => {
+        console.log("dasboard: ", data);
+        let messaggio = ``
+        if (data.city !== null && data.country !== null) {
+            messaggio += `${data.city} si trova in ${data.country}. \n`
+        }
+        if (data.temperature !== null && data.weather !== null) {
+            messaggio += `oggi ci sono ${data.temperature} gradi e il clima é ${data.weather}. \n`
+        }
+        if (data.airport !== null) {
+            messaggio += `l'aeroporto principale é ${data.airport}. \n`
+        }
+        console.log(messaggio)   
+})
+.catch (error => console.error(error));
